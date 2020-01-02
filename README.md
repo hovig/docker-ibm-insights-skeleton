74ae98a2a9244f2af29606a2afe63e6965233d16
hovig <hovigg@hotmail.com>
2020-01-02 15:18:05 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # docker-ibm-insights-skeleton
+
+### Docker compose template for a project
+
+'''
+docker-compose up
+'''
+
+> In a new terminal
+
+'''
+cd aggregation && docker build -t insights . && docker run insights
+'''
---
+++ web/app.js
@@ -0,0 +1,37 @@
+var cors = require( 'cors' );
+var Database = require( 'better-sqlite3' );
+var express = require( 'express' );
+var jsonfile = require( 'jsonfile' );
+var parser = require( 'body-parser' );
+var request = require( 'sync-request' );
+
+
+// Application
+var app = express();
+
+// IP tracking
+app.enable( 'trust proxy' );
+
+// Cross-domain
+app.use( cors() );
+
+// Middleware
+app.use( parser.json( {limit: '50mb'} ) );
+app.use( parser.urlencoded( {
+	limit: '50mb',
+	extended: false,
+	parameterLimit: 50000
+} ) );
+
+
+
+// Static for main files
+app.get('/', (req, res) => {
+	res.send('web @ port 5000');
+});
+
+// Listen
+var server = app.listen( 5000, function() {
+	// Debug
+	console.log("Server started at localhost:5000");
+});
